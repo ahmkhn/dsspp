f607fd48e0b9f99e8904f5d4a99d16e7f055b13c
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-07-15 20:55:46 -0400

    add: homepage button and dssp title

--- dssp/app/map/page.tsx
+++ dssp/app/map/page.tsx
@@ -8,9 +8,15 @@ export default async function Map(){
   return(
     <>
         <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <nav className="w-full flex justify-center border-b border-gray-600 h-16 items-center font-bold text-4xl">
-                <div className="flex justify-center flex-grow mx-6">
-                    Interactive Map
+            <nav className="w-full border-b border-gray-600 h-16 flex font-bold text-4xl">
+                <div className="flex justify-between items-center flex-grow mx-6">
+                <a
+                    className="inline-flex items-center justify-center bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-lg font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground h-11 px-2 rounded-md"
+                    href="/">
+                    Homepage
+                </a>
+                    <h1>Interactive Map</h1>
+                    <p>DSSP</p>
                 </div>
             </nav>
             <main className="flex flex-grow flex-col gap-6 w-2/3 h-[70vh] max-h-full border-black rounded-lg margin" style={{borderRadius: '8px', overflow: 'hidden'}}>
