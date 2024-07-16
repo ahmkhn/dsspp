30b34b0689920a6b4b4ee3445fe3f392ebb2c10e
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-07-15 20:11:35 -0400

    auth working

---
+++ dssp/app/login/oauth-signin.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Provider } from "@supabase/supabase-js";
+import { oAuthSignIn } from "./actions";
+import Link from "next/link";
+type OAuthProvider = {
+  name: Provider;
+  displayName: string;
+  icon?: JSX.Element;
+};
+
+
+export function OAuthButtons() {
+  const oAuthProviders: OAuthProvider[] = [
+    {
+      name: "google",
+      displayName: "Google",
+      icon: <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 48 48"
+      width="24px"
+      height="24px"
+    >
+      <path
+        fill="#4285F4"
+        d="M45.12 24.5c0-1.4-.12-2.42-.38-3.49H24v6.96h11.95c-.25 2.09-1.64 5.21-4.72 7.31l-.04.26 6.87 5.3.48.05c4.36-4.01 6.88-9.9 6.88-16.39z"
+      />
+      <path
+        fill="#34A853"
+        d="M24 48c6.3 0 11.59-2.07 15.46-5.63L31.8 37.1c-2.1 1.44-4.81 2.32-7.8 2.32-5.99 0-11.07-4.06-12.88-9.57l-.25.02-7.14 5.55-.1.25C7.62 44.3 15.33 48 24 48z"
+      />
+      <path
+        fill="#FBBC05"
+        d="M11.12 29.85A14.85 14.85 0 0 1 10 24c0-1.71.29-3.36.8-4.85L4.02 13.55 3.83 13A24 24 0 0 0 0 24c0 3.78.86 7.34 2.4 10.49l8.72-6.64z"
+      />
+      <path
+        fill="#EA4335"
+        d="M24 9.5c4.42 0 7.42 1.9 9.12 3.47l6.64-6.64C35.58 2.59 30.3 0 24 0 15.33 0 7.62 3.7 3.04 9.25l8.82 6.86c1.81-5.51 6.89-9.57 12.88-9.57 2.99 0 5.7.88 7.8 2.32L39.46 5.63C35.59 2.07 30.3 0 24 0z"
+      />
+    </svg>
+    },
+  ];
+
+  return (
+    <>
+      {oAuthProviders.map((provider) => (
+        <button
+          className="w-full flex items-center justify-center gap-2"
+          onClick={async () => {
+            await oAuthSignIn(provider.name);
+          }}
+        >
+          {provider.icon}
+          Login with {provider.displayName}
+        </button>
+      ))}
+      <Link href="/mapu" className="underline flex justify-center mt-3">View without signing in?</Link>
+      
+    </>
+  );
+}
--- dssp/app/page.tsx
+++ dssp/app/page.tsx
@@ -1,9 +1,11 @@
 import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/asdasd/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import Link from "next/link";
+import { signOut } from "@/app/login/actions";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -17,21 +19,32 @@ export default async function Index() {
     }
   };
 
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-gray-600 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-
-        </div>
+      {user !== null ? (
+            <form action={signOut} className="flex items-center gap-2">
+              <p>{user.email}</p>
+              <button>Sign Out</button>
+            </form>
+          ) : (
+            <button className="hover:bg-white hover:text-black hover:shadow hover:shadow-slate-600">
+              <Link href="/login">Sign In</Link>
+            </button>
+            
+          )}
       </nav>
-
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-
         <div className="flex justify-center space-x-4">
           <a
             className="inline-flex items-center justify-center bg-gradient-to-r from-blue-500 via-pink-500 to-blue-500 text-lg font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground h-11 px-8 rounded-md"
@@ -42,7 +55,10 @@ export default async function Index() {
         </div>
         </main>
       </div>
+      <div>
+        Signed in as:
 
+      </div>
       <footer className="w-full border-t border-gray-600 p-8 flex justify-center text-center text-xs">
         <p>
           Developed by {""}
