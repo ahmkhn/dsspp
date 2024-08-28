d33e4199829d7f8f9d804c0ec4269e8b99acf24b
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-08-28 06:30:49 -0400

    style: z index and punctuation fixes

--- dssp/app/page.tsx
+++ dssp/app/page.tsx
@@ -30,10 +30,10 @@ export default async function Index() {
   
   
 
-  <nav className="z-10 w-full border-b border-gray-600 py-4 flex flex-col sm:flex-row items-center">
+  <nav className="z-20 w-full border-b border-gray-600 py-4 flex flex-col sm:flex-row items-center">
         <div className="flex flex-col sm:flex-row justify-between items-center w-full px-4 sm:px-6 space-y-4 sm:space-y-0">
           <a 
-            className="inline-flex items-center justify-center text-center bg-white text-black px-4 py-2 rounded text-sm sm:text-base font-bold"
+            className="z-50 inline-flex items-center justify-center text-center bg-white text-black px-4 py-2 rounded text-sm sm:text-base font-bold"
             href="/"
           >
             Homepage
@@ -47,7 +47,7 @@ export default async function Index() {
             <></>
           )}
           <a 
-            className="inline-flex items-center justify-center text-center bg-white text-black px-4 py-2 rounded text-sm sm:text-base font-bold"
+            className="z-50 inline-flex items-center justify-center text-center bg-white text-black px-4 py-2 rounded text-sm sm:text-base font-bold"
             href="/about"
           >
             About DSSP
--- dssp/components/Header.tsx
+++ dssp/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       </h1>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-gray-300 to-transparent my-4" />
       <p className="font-black text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
-        A platform for a global community to share <span className="from-blue-500 via-pink-500 to-blue-500 bg-gradient-to-r block text-transparent bg-clip-text">research</span> and <span className="block text-transparent bg-clip-text">collaborate</span>, fostering <span className="block text-transparent bg-clip-text">unity</span> and <span className="block text-transparent bg-clip-text">mutual support.</span>
+        A platform for a global community to share <span className="from-blue-500 via-pink-500 to-blue-500 bg-gradient-to-r block text-transparent bg-clip-text">research</span> and <span className="block text-transparent bg-clip-text">collaborate</span>, fostering <span className="block text-transparent bg-clip-text">unity</span> and <span className="block text-transparent bg-clip-text">mutual support</span>
       </p>
     </div>
   );
