c350e9ed0189ebf6a470e7b3f6149b4af7aea0bb
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-08-28 10:44:45 -0400

    fix: opengraph potential image embed fix for linkedin

--- dssp/app/layout.tsx
+++ dssp/app/layout.tsx
@@ -7,37 +7,26 @@ const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "https://www.dssp.app";
 
-const faviconUrl = `https://www.dssp.app/favicon.ico`;
-
 export const metadata: Metadata = {
   title: 'DSSP',
-  description: "Decolonization of Social Sciences in Pakistan",
+  description: 'Decolonization of Social Sciences in Pakistan',
   openGraph: {
     title: 'DSSP',
     description: 'Decolonization of Social Sciences in Pakistan',
-    url: defaultUrl,
-    siteName: 'DSSP',
+    url: 'https://www.dssp.app',
+    siteName: 'dssp',
     images: [
       {
-        url: faviconUrl,
-        width: 32,
-        height: 32,
-        alt: 'DSSP Favicon',
+        url: 'https://www.dssp.app/og-image.png',
+        width: 1200,
+        height: 630,
       },
     ],
     locale: 'en_US',
     type: 'website',
   },
-  twitter: {
-    card: 'summary', // Changed to 'summary' as we're using a small image
-    title: 'DSSP',
-    description: 'Decolonization of Social Sciences in Pakistan',
-    images: [faviconUrl],
-  },
-  icons: {
-    icon: '/favicon.ico',
-  },
 }
+
 const inter = Inter({subsets:['latin']})
 
 export default function RootLayout({
