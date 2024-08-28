568e7ba3bd5de927ef7df8f1c25c204d7b4231df
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-08-28 10:54:47 -0400

    fix: metadata reset for SEO

--- dssp/app/layout.tsx
+++ dssp/app/layout.tsx
@@ -1,31 +1,15 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import {Inter} from 'next/font/google';
-import type { Metadata } from 'next'
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "https://www.dssp.app";
+const defaultUrl = 'https://www.dssp.app'
 
-export const metadata: Metadata = {
-  title: 'DSSP',
-  description: 'Decolonization of Social Sciences in Pakistan',
-  openGraph: {
-    title: 'DSSP',
-    description: 'Decolonization of Social Sciences in Pakistan',
-    url: 'https://www.dssp.app',
-    siteName: 'dssp',
-    images: [
-      {
-        url: 'https://www.dssp.app/og-image.png',
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-}
+  export const metadata = {
+    metadataBase: new URL(defaultUrl),
+    title: "Decolonizing Social Sciences",
+    description: "DSSP",
+  };
+  
 
 const inter = Inter({subsets:['latin']})
 
