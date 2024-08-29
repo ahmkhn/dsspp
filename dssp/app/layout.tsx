849f25053d2de41bf755c571577b1e4a1c3e6844
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-08-28 22:06:29 -0400

    fix: remove next/head as it's deprecated, using metadata api instead

--- dssp/app/layout.tsx
+++ dssp/app/layout.tsx
@@ -1,7 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import {Inter} from 'next/font/google';
-import Head from "next/head";
 const defaultUrl = 'https://www.dssp.app'
 
   export const metadata = {
@@ -21,13 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <Head>
-        <meta property="og:image" content="https://www.dssp.app/twitter-image.png"></meta>
-        <meta property="og:title" content="DSSP" />
-        <meta property="og:description" content="Decolonization of Social Sciences in Pakistan" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://dssp.app" />
-      </Head>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
