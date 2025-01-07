4734742b8602f7d82abc333641e7c69a0d987776
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2025-01-06 23:34:17 -0500

    style: map height

--- dssp/app/map/page.tsx
+++ dssp/app/map/page.tsx
@@ -7,19 +7,18 @@ export default async function Map(){
     data: { user },
   } = await supabase.auth.getUser();
   return (
-    <div className="flex-1 w-full flex flex-col min-h-screen">
+    <div className="flex-1 w-full flex flex-col h-screen ">
       <Nav/>
-      <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center">Interactive Map</h1>
       <main className="flex-1 flex flex-col">
         <section className="flex-1 w-full flex flex-col p-4">
-          <div className="flex h-full">
-            <div className="flex-grow h-[85vh] border-black rounded-lg" style={{borderRadius: '8px', overflow: 'hidden'}}>
+          <div className="flex flex-1">
+            <div className="flex-grow border-black rounded-lg" style={{borderRadius: '8px', overflow: 'hidden'}}>
               <Worldmap authorized={user} />
             </div>
           </div>
         </section>
       </main>
-      <footer className="w-full border-t border-gray-600 p-4 flex justify-center text-center text-xs">
+      <footer className="w-full border-t border-gray-600 p-2 flex justify-center text-center text-xs">
         <p>
           Developed by {" "}
           <a href="https://linkedin.com/in/ahmkhn" target="_blank" className="text-green-500 font-bold underline" rel="noreferrer">
